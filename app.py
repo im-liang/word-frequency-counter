300e5457ebfbed7a72ec8b6b1f8e1e409583d70e
Jie Liang <jieliang456@gmail.com>
2017-10-12 12:12:11 -0400

    bug fix on searching wor in dir

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import support
 import const
 
 def wordFrequency(fname):
+    """
+    Analysis of word usage for the file
+    @param fname: file name
+    @return: word Frequency
+    """
     counter = {}
     content = support.convertFile(fname)
     sentences = support.split2Centences(content)
@@ -19,30 +24,31 @@ def wordFrequency(fname):
     return counter
 
 
-def mostFrequentWord(counter):
+def mostFrequentWordinFile(counter):
     max_count = 0
     result = {}
-    if type(counter) is dict: # single file
-        for word in counter:
-            if (len(counter[word]) > max_count):
-                result.clear()
-                result[word] = counter[word]
-                max_count = len(counter[word])
-            elif (len(counter[word]) == max_count):
-                result[word] = counter[word]
-    else:
-        for x in counter:
-            for word in x:
-                if (len(x[word]) > max_count):
-                    result.clear()
-                    result[word] = x[word]
-                    max_count = len(x[word])
-                elif (len(x[word]) == max_count):
-                    result[word] = x[word]
-
+    for word in counter:
+        if (len(counter[word]) > max_count):
+            result.clear()
+            result[word] = counter[word]
+            max_count = len(counter[word])
+        elif (len(counter[word]) == max_count):
+            result[word] = counter[word]
     print "{:<15} {:<20} {:<20}".format('Word','Frequency','Sentences')
     for current_word in result:
         print "{:<15} {:<20} {:<20}".format(current_word, max_count, result[current_word])
+
+
+def mostFrequentWordinDir(counters):
+    total_counter = {}
+    for counter in counters:
+        for word in counter:
+            if word in total_counter:
+                total_counter[word] = total_counter[word] + counter[word]
+            else:
+                total_counter[word] = counter[word]
+
+    mostFrequentWordinFile(total_counter)
 
 
 def main():
@@ -50,10 +56,10 @@ def main():
         files = [sys.argv[1] +'/' + f for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]
         pool = Pool(len(files))
         counter = pool.map(wordFrequency, files)
-        mostFrequentWord(counter)
+        mostFrequentWordinDir(counter)
     elif exists(sys.argv[1]):
         counter = wordFrequency(sys.argv[1])
-        mostFrequentWord(counter)
+        mostFrequentWordinFile(counter)
     else:
         print const.INPUT_ERR_MSG
         return
